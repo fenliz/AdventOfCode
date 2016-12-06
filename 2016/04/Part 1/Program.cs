ac3843088d40779987ab1d27ff2a2f0550a5da10
Charlie Forsberg Hedberg <charlie.forsberg.hedberg@ifsworld.com>
2016-12-06 09:29:26 +0100

    Start 04 Part 1

---
+++ 2016/04/Part 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode04_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public IOrderedEnumerable<KeyValuePair<char, int>> GetCharCount(string value)
+        {
+            return value.GroupBy(c => c)
+                        .ToDictionary(grp => grp.Key, grp => grp.Count())
+                        .OrderBy(c => c.Value);
+        }
+
+        public static bool IsValidChecksum(string checksum)
+        {
+            char previousChar = checksum[0];
+            for(int i = 1; i < checksum.Length; i++)
+            {
+                if(checksum[i] < previousChar)
+                    return false;
+                previousChar = checksum[i];
+            }
+            return true;
+        }
+    }
+}
