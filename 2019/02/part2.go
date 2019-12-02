55514814e2678ed8d7bfa3cf0f2ff94d94d90c11
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2019-12-02 19:21:35 +0100

    day2 part2

--- 2019/02/part2.go
+++ 2019/02/part2.go
@@ -1,30 +1,45 @@
 package main
 
+import "fmt"
+
 func Part2(numbers []int) int {
-	for i := 0; i < len(numbers); {
-		firstIndex := numbers[i+1]
-		secondIndex := numbers[i+2]
-		resultIndex := numbers[i+3]
+	for x := 0; x < 99; x++ {
+		for y := 0; y < 99; y++ {
+			localNumbers := make([]int, len(numbers))
+			copy(localNumbers, numbers)
 
-		firstValue := numbers[firstIndex]
-		secondValue := numbers[secondIndex]
+			localNumbers[1] = x
+			localNumbers[2] = y
 
-		var result int
-		switch numbers[i] {
-		case 1:
-			result = firstValue + secondValue
-			i += 4
-		case 2:
-			result = firstValue * secondValue
-			i += 4
-		default:
-			i++
-		}
-		numbers[resultIndex] = result
+			for i := 0; i < len(localNumbers); {
+				firstIndex := localNumbers[i+1]
+				secondIndex := localNumbers[i+2]
+				resultIndex := localNumbers[i+3]
 
-		if result == 19690720 {
-			return 100*firstValue + secondValue
+				firstValue := localNumbers[firstIndex]
+				secondValue := localNumbers[secondIndex]
+
+				var result int
+				switch localNumbers[i] {
+				case 1:
+					result = firstValue + secondValue
+					i += 4
+				case 2:
+					result = firstValue * secondValue
+					i += 4
+				default:
+					i++
+				}
+				localNumbers[resultIndex] = result
+
+				if result == 19690720 {
+					fmt.Println(x)
+					fmt.Println(y)
+					return 100*x + y
+				}
+			}
 		}
 	}
+
 	return -1
 }
